b507a6c5a8207c74d036b0054e2a4fa023324453
Norbert Huffschmid <norbert.huffschmid@long-exposure.net>
2015-02-22 15:12:09 +0100

    first working dbus draft (with thread issues)

--- python/potsbliz/__main__.py
+++ python/potsbliz/__main__.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
+import dbus
+import dbus.mainloop.glib
 import signal
 import sys
 import time
+from gi.repository import GLib
 from threading import Event
 from potsbliz.logger import Logger
 from potsbliz.state_machine import StateMachine
@@ -13,8 +16,11 @@ class Potsbliz(object):
     def run(self):
         with Logger(__name__ + '.run') as log:
             
+            dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+            #dbus.mainloop.glib.init_threads()
+
             with StateMachine():
-                
+                '''
                 # register for SIGTERM
                 self._sigterm_event = Event()
                 signal.signal(signal.SIGTERM, lambda signum, frame: self._sigterm_event.set())
@@ -22,6 +28,16 @@ class Potsbliz(object):
                 self._sigterm_event.wait(sys.maxint)
                 
                 log.info('SIGTERM event received. Shutting down ...')
+                '''
+                
+                # register for SIGTERM
+                signal.signal(signal.SIGTERM, lambda signum, frame: self._mainloop.quit())
+
+                self._mainloop = GLib.MainLoop()
+                GLib.threads_init()
+                log.debug('Run mainloop ...')
+                self._mainloop.run() 
+                log.debug('Mainloop terminated.')
 
 
 if __name__ == '__main__':
