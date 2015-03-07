c25f8d3621424d866ee811f0ce345d253fb7b3e9
Norbert Huffschmid <norbert.huffschmid@long-exposure.net>
2015-03-07 15:51:47 +0100

    added webbased deletion of bluetooth devices

--- apache2/www/bluetooth.py
+++ apache2/www/bluetooth.py
@@ -5,21 +5,28 @@ from mod_python import apache
 def list(req):
     req.content_type = 'application/json; charset=UTF8'
 
+    rows = []
     bus = dbus.SystemBus()
     manager = dbus.Interface(bus.get_object('org.bluez', '/'), 'org.freedesktop.DBus.ObjectManager')
     objects = manager.GetManagedObjects()
-
-    rows = []
     for path in objects.keys():
         interfaces = objects[path]
         for interface in interfaces.keys():
             if (interface == 'org.bluez.Device1'):
                 properties = interfaces[interface]
-                rows.append({ 'modemname': path, 'name': properties['Name'], 'connected': properties['Connected'] })
+                rows.append({ 'device': path, 'name': properties['Name'], 'connected': properties['Connected'] })
 
-    return json.dumps({ "Result": "OK", "Records": rows })
+    return json.dumps({ 'Result': 'OK', 'Records': rows })
 
 
 def delete(req):
-    result = { "Result": "OK" }    
-    return json.dumps(result)
+    req.content_type = 'application/json; charset=UTF8'
+    
+    # read POST parameter
+    device = req.form.getfirst('device')
+
+    bus = dbus.SystemBus()
+    adapter = dbus.Interface(bus.get_object('org.bluez', '/org/bluez/hci0'), 'org.bluez.Adapter1')
+    adapter.RemoveDevice(device)
+
+    return json.dumps({ "Result": "OK" })
