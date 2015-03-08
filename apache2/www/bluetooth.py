85ae7a6215c795a91b9cfb2d360508164027fd27
Norbert Huffschmid <norbert.huffschmid@long-exposure.net>
2015-03-08 13:35:10 +0100

    added device pairing

--- apache2/www/bluetooth.py
+++ apache2/www/bluetooth.py
@@ -14,7 +14,10 @@ def list(req):
         for interface in interfaces.keys():
             if (interface == 'org.bluez.Device1'):
                 properties = interfaces[interface]
-                rows.append({ 'device': path, 'name': properties['Name'], 'connected': properties['Connected'] })
+                rows.append({ 'device': path,
+                              'name': properties['Name'],
+                              'connected': properties['Connected'],
+                              'paired': properties['Paired'] })
 
     return json.dumps({ 'Result': 'OK', 'Records': rows })
 
@@ -29,4 +32,32 @@ def delete(req):
     adapter = dbus.Interface(bus.get_object('org.bluez', '/org/bluez/hci0'), 'org.bluez.Adapter1')
     adapter.RemoveDevice(device)
 
+    return json.dumps({ "Result": "OK" })
+
+
+def start_discovery(req):
+    bus = dbus.SystemBus()
+    adapter = dbus.Interface(bus.get_object('org.bluez', '/org/bluez/hci0'), 'org.bluez.Adapter1')
+    adapter.StartDiscovery()
+
+
+def stop_discovery(req):
+    bus = dbus.SystemBus()
+    adapter = dbus.Interface(bus.get_object('org.bluez', '/org/bluez/hci0'), 'org.bluez.Adapter1')
+    adapter.StopDiscovery()
+    
+    
+def pair(req):
+    req.content_type = 'application/json; charset=UTF8'
+    
+    # read POST parameter
+    device_id = req.form.getfirst('device')
+
+    bus = dbus.SystemBus()
+    device = dbus.Interface(bus.get_object('org.bluez', device_id), 'org.bluez.Device1')
+    device.Pair()
+    properties_manager = dbus.Interface(device, 'org.freedesktop.DBus.Properties')
+    properties_manager.Set('org.bluez.Device1', 'Trusted', True)
+    device.Connect()
+    
     return json.dumps({ "Result": "OK" })
