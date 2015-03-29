b76da47158d3398ff81f8340fa80eb5253d3730e
Norbert Huffschmid <norbert.huffschmid@long-exposure.net>
2015-03-29 13:02:28 +0200

    modified version info handling

--- apache2/www/info.py
+++ apache2/www/info.py
@@ -6,9 +6,15 @@ from subprocess import Popen, PIPE
 def index(req):
     req.content_type = 'text/plain'
     
-    process = Popen(['/usr/bin/apt-cache', 'show' ,'potsbliz-rpi'], stdout=PIPE, stderr=PIPE)
-    output, errors = process.communicate()
-    req.write(output)
+    req.write('POTSBLIZ - (C)2015 - Norbert Huffschmid\n\n')
+
+    req.write('Plain Old Telephone Service Beyond Local IP Stack\n\n')
+    
+    with open ('/usr/local/lib/python2.7/dist-packages/potsbliz/potsbliz.version', 'r') as version_file:
+        version = version_file.read()
+    req.write('Version: ' + version + '\n')
+
+    req.write('License: GNU GPL V3\n\n')
     
     process = Popen(['/bin/uname', '-a'], stdout=PIPE, stderr=PIPE)
     output, errors = process.communicate()
